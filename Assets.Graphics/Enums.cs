65d589ff311fddb8c1e60eb812d13c8f5731f596
niii.nonno@gmail.com <niii.nonno@gmail.com>
2022-09-23 09:21:28 +0900

    Assets.Graphicsの追加。

---
+++ Assets.Graphics/Enums.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nonno.Assets.Graphics;
+
+[Serializable]
+[Flags]
+public enum Direction
+{
+    // 横向き。x軸。第一変数自由。
+    Horizontal = 0b0001,
+    // 縦向き。y軸。第二変数自由。
+    Vertical = 0b0010,
+
+}
+
+[Serializable]
+[Flags]
+public enum Position
+{
+    /// <summary>
+    /// 中央。
+    /// </summary>
+    Center = 0b0000,
+    /// <summary>
+    /// 左。
+    /// </summary>
+    Left = 0b0010,
+    /// <summary>
+    /// 右。
+    /// </summary>
+    Right = 0b0001,
+    /// <summary>
+    /// 横軸を指定しない。
+    /// </summary>
+    HorizontalFree = Left | Center | Right,
+    /// <summary>
+    /// 上。
+    /// </summary>
+    Top = 0b1000,
+    /// <summary>
+    /// 下。
+    /// </summary>
+    Bottom = 0b0100,
+    /// <summary>
+    /// 縦軸を指定しない。
+    /// </summary>
+    VerticalFree = Top | Center | Bottom,
+}
---
+++ Assets.Graphics/IVideoSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonno.Assets.Graphics;
+
+public interface IVideoSource
+{
+    decimal Interval { get; }
+
+    void Seek(long number);
+
+    Task CopyNextFrame(Bitmap to);
+}
