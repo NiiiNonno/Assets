591be41126133bd79e3a78e21fbcc29dbf5f60e8
niii.nonno@gmail.com <niii.nonno@gmail.com>
2022-09-23 09:29:38 +0900

    Assets.Razorsの追加。

---
+++ Assets.Razors/InputPoint.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Nonno.Assets.Graphics;
+using Range = Nonno.Assets.Graphics.Range;
+
+namespace Nonno.Assets.Razors
+{
+    partial class InputPoint
+    {
+        public int X
+        {
+            get => Value.X;
+            set
+            {
+                Value = new Point(value, Y);
+                _ = ValueChanged?.InvokeAsync(Value);
+            }
+        }
+        public int Y
+        {
+            get => Value.Y;
+            set
+            {
+                Value = new Point(X, value);
+                _ = ValueChanged?.InvokeAsync(Value);
+            }
+        }
+        [Parameter]
+        public int Step { get; set; }
+        [Parameter]
+        public string? Name { get; set; }
+        [Parameter]
+        public Point Value { get; set; }
+        [Parameter]
+        public EventCallback<Point>? ValueChanged { get; set; }
+    }
+}
