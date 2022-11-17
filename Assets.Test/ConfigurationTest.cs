9273a7501cbc9287c248d7bc09ebc809d3710778
niii.nonno@gmail.com <niii.nonno@gmail.com>
2022-11-17 11:48:58 +0900

    追加、設定の試験。

---
+++ Assets.Test/ConfigurationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Nonno.Assets.Collections;
+
+namespace Nonno.Assets.Test;
+[TestClass]
+public class ConfigurationTest
+{
+    [TestMethod]
+    public void Use()
+    {
+    }
+}
