b4137224c866313e08755b7d916c5bf1ad37bb89
niii.nonno@gmail.com <niii.nonno@gmail.com>
2023-03-08 16:14:15 +0900

    簡単(simple)逍囲(spin lock)の実装。

---
+++ Assets/SimpleSpinLock.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Nonno.Assets;
+
+public struct SimpleSpinLock
+{
+	const int TRUE = 1;
+	const int FALSE = 0;
+	volatile int _isLocked;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Enter()
+	{
+		if (Interlocked.Or(ref _isLocked, TRUE) == TRUE)
+		{
+			Spin();
+		}
+		return;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Exit() => _isLocked = FALSE;
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	void Spin()
+	{
+		var sw = new SpinWait();
+		do
+		{
+			sw.SpinOnce();
+		}
+		while (Interlocked.Or(ref _isLocked, TRUE) == TRUE);
+	}
+}
