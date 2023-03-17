2ddc34de4591a4ee8758ada467b5740b2fea464b
niii.nonno@gmail.com <niii.nonno@gmail.com>
2023-03-17 13:57:48 +0900

    資属録と句凡属録の統合。 基色の整序。 瀏刀制御卓句の実装。

--- Assets/Enums.cs
+++ Assets/Enums.cs
@@ -1,5 +1,7 @@
 ﻿// 令和弐年大暑確認済。
 
+using System.Collections.Immutable;
+
 namespace Nonno.Assets;
 
 public enum ComparisonResult
@@ -16,16 +18,39 @@ public enum BasicColor : byte
     Red = 0b0000_0001,
     Green = 0b0000_0010,
     Blue = 0b0000_0100,
-    Thin = 0b0000_1000,
+    Black = 0b0000_1000,
 
     Yellow = Red | Green,
     Cyan = Green | Blue,
     Magenta = Blue | Red,
+    White = Red | Green | Blue,
 
+    Thin = Black,
     ThinRed = Red | Thin,
     ThinGreen = Green | Thin,
     ThinBlue = Blue | Thin,
     ThinYellow = Yellow | Thin,
     ThinCyan = Cyan | Thin,
     ThinMagenta = Magenta | Thin,
+
+    Gray = White | Black,
+
+    /*
+     * none
+     * red
+     * green
+     * yellow
+     * blue
+     * magenta
+     * cyan
+     * white
+     * black thin
+     * ...
+     */
+}
+
+public readonly record struct ColoredCharacter(char Character, BasicColor ForegroundColor = BasicColor.None, BasicColor BackgroundColor = BasicColor.None)
+{
+    public static implicit operator ColoredCharacter(char value) => new(value);
+    public static explicit operator char(ColoredCharacter value) => value.Character;
 }
