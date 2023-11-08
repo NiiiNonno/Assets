cbab6f5539a961dfac632618cec7892bbb53a3f6
新生のんの <74212356+NiiiNonno@users.noreply.github.com>
2023-11-09 08:13:31 +0900

    表の拡張。
    `Nonno.Text.ITable`との統合を図る。

--- Assets/Collections/ITable.cs
+++ Assets/Collections/ITable.cs
@@ -5,7 +5,15 @@ using System.Text;
 namespace Nonno.Assets.Collections;
 public interface ITable<TKey, TValue>
 {
-    TValue? this[TKey key] { get; set; }
+    TValue? this[TKey key]
+    {
+        get => TryGetValue(key, out var r) ? r : throw new KeyNotFoundException();
+        set { if (!TrySetValue(key, value)) throw new KeyNotFoundException(); }
+    }
     
     ISet<TKey> Keys { get; }
+
+    bool ContainsKey(TKey key) => Keys.Contains(key)
+    bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value);
+    bool TrySetValue(TKey key, TValue value);
 }
