df9394d2a8fcb2f0ecfbe2017857d65fe6acf479
JSACL <j.s.and.c.lover@gmail.com>
2023-11-09 08:46:56 +0900

    皆賓の追加。

---
+++ Assets.Collections/WholeSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nonno.Assets.Collections;
+public class WholeSet<T> : ISet<T>
+{
+    private WholeSet() { }
+
+    public bool Contains(T item) => true;
+    public IEnumerator<T> GetEnumerator() => Enumerator ?? throw new NotSupportedException();
+
+    public static IEnumerator<T>? Enumerator { get; set; }
+    public static WholeSet<T> Instance { get; }
+
+    static WholeSet()
+    {
+        Instance = new();
+
+        switch (default(T))
+        {
+        case int:
+        default:
+            break;
+        }
+    }
+}
